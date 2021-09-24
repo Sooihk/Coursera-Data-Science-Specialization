bb0f9e494f6db1b9d556067002c7f3672699fcf6
Sooihk Ro <67810593+Sooihk@users.noreply.github.com>
2021-09-23 18:31:39 -0700

    Update plot1.R

--- Exploratory Data Analysis/Project 2/plot1.R
+++ Exploratory Data Analysis/Project 2/plot1.R
@@ -1,1 +1,12 @@
+source("PMZIPfile")
 
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#sum emissions per year
+emissions_per_year <- aggregate(Emissions ~ year, NEI, FUN=sum)
+png("plot1.png")
+barplot(emissions_per_year$Emissions/1000, names.arg=emissions_per_year$year, xlab="Year", 
+        ylab=expression('PM'[2.5]*' in Kilotons'), main=expression('Annual PM'[2.5]*' Emission from 1999 to 2008'),
+        col=as.factor(emissions_per_year$year))
+dev.off()
